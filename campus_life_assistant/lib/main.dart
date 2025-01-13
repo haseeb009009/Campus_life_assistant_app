c31f6760cd361bc93d6ea90e69fc5fc78a828ac9
Haseeb-ur-Rehman/009 <choudharyhaseeb86@gmail.com>
2025-01-13 11:24:09 +0500

    images

--- campus_life_assistant/lib/main.dart
+++ campus_life_assistant/lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Campus Life Assistant',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const LoginPage(),
